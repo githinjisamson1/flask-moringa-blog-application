877778d39559c94d0aac3d7e2462da912d6ce034
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-24 15:22:04 +0300

    Register user_bp, post_bp, comment_bp, vote_bp

--- server/app.py
+++ server/app.py
@@ -1,26 +1,28 @@
-from server.config import app, api
 from flask import make_response, jsonify
 from flask_restful import Resource
+from server.config import app, api
 from server.controllers.user_controllers import user_bp
+from server.controllers.post_controllers import post_bp
+from server.controllers.comment_controllers import comment_bp
+from server.controllers.vote_controllers import vote_bp
 
+# !register blueprints
 app.register_blueprint(user_bp)
+app.register_blueprint(post_bp)
+app.register_blueprint(comment_bp)
+app.register_blueprint(vote_bp)
 
 
-# !RESOURCES
+# resources
 class Index(Resource):
-    # !GET
     def get(self):
-        response = make_response(jsonify({
+        return {
             "success": True,
             "message": "Welcome to Thee Moringa Blog!"
-        }), 200)
-
-        response.headers["Content-Type"] = "application/json"
-
-        return response
+        }, 200
 
 
-# !ROUTES
+# resource + route
 api.add_resource(Index, "/")
 
 
