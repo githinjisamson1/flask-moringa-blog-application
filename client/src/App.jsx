79b6dba6fbef2feb5df68e9efacd2f51bb7f345b
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-25 16:16:15 +0300

    Add signup and signin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
+import Signup from "./components/home/Signup";
+import Signin from "./components/home/Signin";
+// npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/signin" element={<Signin />} />
       </Routes>
     </div>
   );
