3f52ae5ba3c3ca2c173e6aaf787721dd14b2407d
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-27 12:34:49 +0300

    Add dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Signup from "./components/signup/Signup";
-import Signin from './components/signin/Signin';
-// npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+import Signin from "./components/signin/Signin";
+import Dashboard from "./components/dashboard/Dashboard";
 
+// routes
 const App = () => {
   return (
     <div>
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/signin" element={<Signin />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
