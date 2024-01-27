76ef8632244fc944e97f0f65e191cbd01f486f38
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-27 12:32:35 +0300

    Add pizzasContext

---
+++ client/src/context/postsContext.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState, useContext } from "react";
+
+// PostsContext
+const PostsContext = React.createContext();
+
+// PostsProvider
+const PostsProvider = ({ children }) => {
+  // states
+  const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState(posts ? posts : []);
+
+  // handle filter posts by phase
+  const filterPostByPhase = (phase) => {
+    const phasePosts = posts.filter((post) => post.phase === phase);
+    setFilteredPosts(phasePosts);
+  };
+
+  // wrap return using Provider
+  return (
+    <PostsContext.Provider
+      value={{
+        posts,
+        setPosts,
+        filterPostByPhase,
+        filteredPosts,
+        setFilteredPosts,
+      }}
+    >
+      {children}
+    </PostsContext.Provider>
+  );
+};
+
+// useGlobalContext can be accessed globally
+export const useGlobalContext = () => {
+  return useContext(PostsContext);
+};
+
+export { PostsContext, PostsProvider };
