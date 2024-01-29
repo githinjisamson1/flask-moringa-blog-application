bbf43d81797cf4e404c3c8631114fcfe18ed06f2
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-29 07:13:05 +0300

    Add createPost/ route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/home/Home";
 import Signup from "./components/signup/Signup";
 import Signin from "./components/signin/Signin";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreatePost from "./components/createPost/CreatePost";
 
 // routes
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/signin" element={<Signin />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/createPost" element={<CreatePost />} />
       </Routes>
     </div>
   );
