c193d7673711db197f853fcedd67a9a9ad88b30a
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-29 23:45:01 +0300

    Complete and style UserAccount

---
+++ client/src/components/userAccount/UserAccount.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./userAccount.css";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useGlobalUserContext } from "../../context/authContext";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const navigate = useNavigate();
+
+  const { currentUser } = useGlobalUserContext();
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      full_name: "",
+      email: "",
+      password: "",
+    },
+
+    validationSchema: yup.object().shape({
+      username: yup.string(),
+      full_name: yup.string(),
+      email: yup.string().email("Invalid email"),
+      password: yup.string().min(8, "Password must be at least 8 characters!"),
+    }),
+
+    onSubmit: (values) => {
+      console.log(values);
+
+    //   fetch API
+      fetch(`/users/${currentUser.data.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("auth_token")}`,
+        },
+        body: JSON.stringify(values),
+      })
+        .then((response) => {
+          if (response.ok) {
+            alert("Account updated successfully");
+            navigate("/profile");
+            return response.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(`Error ${error}`);
+        });
+    },
+  });
+  return (
+    <div className="user-account-container">
+      <form action="" className="user-patch" onSubmit={formik.handleSubmit}>
+        <h1>Personal Info</h1>
+        <h2>Username</h2>
+        <input
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.username}
+          id="username"
+          name="username"
+          className="input"
+        />
+        <h2>Full Name</h2>
+        <input
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.full_name}
+          id="full_name"
+          name="full_name"
+          className="input"
+        />
+        <h2>Email</h2>
+        <input
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          id="email"
+          name="email"
+          className="input"
+        />
+        <h2>Password</h2>
+        <input
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          id="password"
+          name="password"
+          className="input"
+        />
+        <br />
+        <button className="btn" type="submit">
+          Update
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ client/src/components/userAccount/UserAccountPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../header/Header";
+import UserAccount from "./UserAccount";
+
+const UserAccountPage = () => {
+  return (
+    <div>
+      <Header />
+      <UserAccount />
+    </div>
+  );
+};
+
+export default UserAccountPage;
